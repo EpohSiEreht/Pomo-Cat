def2f1e33226f0ed990f142b3163c39b66a580ff
David Lee <ddlee.907@gmail.com>
2016-02-27 16:25:01 -0500

    fix reset function and add comments

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
-
+// Global variables with inital values
 var startTime = {},
     now       = new Date().getTime(),
     min       = 0,
     sec       = 0,
     mField    = 0,
     sField    = 0;
-
+// Insert current time into the startTime variable
 startTime = now;
-
+// Starts the countdown
 function timer() {
-  now = new Date().getTime();
+  now = new Date().getTime();  // Every time the timer function runs, start with new  time
   var display = document.querySelector('#timer'),
       difference = Math.floor(now - startTime);
   if(difference <= 1500000) {
@@ -30,19 +30,18 @@ function timer() {
       display.innerHTML = mField + ":" + sField;
   } 
   else if(difference >= 1500000){
-    now = new Date().getTime();
-    clearInterval(countdown);
+    clearInterval(counter);
     display.innerHTML = "00:00";
   }
 }
-
+// Listen for the start button click
 function start() {
   document.getElementById('start').addEventListener('click', function(){
     counter = setInterval(timer, 1000);
     timer();
   });
 }
-
+// Listen for the stop button click
 function stop() {
   document.getElementById('stop').addEventListener('click', function(){
     var display = document.querySelector('#timer');
@@ -51,30 +50,30 @@ function stop() {
     display.innerHTML = "Fail No Good";
   });
 }
-
+// Listen for the reset button click
 function reset() {
   document.getElementById('reset').addEventListener('click', function(){
     var display = document.querySelector('#timer');
+    clearInterval(counter);
     startTime = {},
     now       = new Date().getTime(),
     min       = 0,
     sec       = 0,
     mField    = 0,
-    sField    = 0;
-    startTime = now,
-    counter   = "";
+    sField    = 0,
+    startTime = now;
     display.innerHTML = "25:00";
   });
 }
-
-function initiatePomodoro(){
+// Set up the Pomodoro
+function initializePomodoro(){
   var counter;
   start();
   stop();
   reset();
 }
-
+// After the page has been loaded, initialize the Pomodoro
 window.onload = function(e) {
   e.preventDefault();
-  initiatePomodoro();
+  initializePomodoro();
 };
